79c5a18a89a67c333ce66c10edb1219f58cacbf4
redefinew <39209908+redefinew@users.noreply.github.com>
2019-04-19 18:08:06 +0700

    Update Block list app.py
    Block a list app NOX player
    lolipop - block
    over lolipop- hide

--- nox/Block list app.py
+++ nox/Block list app.py
@@ -2,31 +2,33 @@
 
 import subprocess
 
-str = """adb shell pm hide com.android.providers.contacts
-adb shell pm hide com.svox.pico
-adb shell pm hide com.android.bluetooth
-adb shell pm hide com.android.wallpaper.livepicker
-adb shell pm hide com.android.soundrecorder
-adb shell pm hide com.google.android.gsf.login
-adb shell pm hide com.android.providers.userdictionary
-adb shell pm hide com.android.browser
-adb shell pm hide com.google.android.play.games
-adb shell pm hide com.android.providers.telephony
-adb shell pm hide com.android.keychain
-adb shell pm hide com.android.webview
-adb shell pm hide com.android.contacts
-adb shell pm hide com.google.android.gsf
-adb shell pm hide com.android.providers.downloads
-adb shell pm hide com.android.htmlviewer
-adb shell pm hide com.facebook.lite
-adb shell pm hide com.android.gallery3d
-adb shell pm hide com.android.soundrecorder
-adb shell pm hide com.android.phone
-adb shell pm hide com.android.mms.service
-adb shell pm hide com.android.server.telecom
-adb shell pm hide com.android.vending
+str = """com.android.providers.contacts
+com.svox.pico
+com.android.bluetooth
+com.android.wallpaper.livepicker
+com.android.soundrecorder
+com.google.android.gsf.login
+com.android.providers.userdictionary
+com.android.browser
+com.google.android.play.games
+com.android.providers.telephony
+com.android.keychain
+com.android.webview
+com.android.contacts
+com.google.android.gsf
+com.android.providers.downloads
+com.android.htmlviewer
+com.facebook.lite
+com.android.gallery3d
+com.android.soundrecorder
+com.android.phone
+com.android.mms.service
+com.android.server.telecom
+com.android.vending
 """
+
+you_want_to = 'block'
+
 s=str.splitlines()
 for x in s:
-    print(x.split()) 
-    subprocess.call(x.split())
+    subprocess.Popen('adb shell pm '+ you_want_to + x)
